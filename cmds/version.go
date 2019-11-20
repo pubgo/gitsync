15d244a1119d7d044c6441beecdd000c19a23839
kooksee <kooksee@163.com>
2019-11-20 22:29:25 +0800

    fix: update version

--- cmds/version.go
+++ cmds/version.go
@@ -5,6 +5,7 @@ import (
 	"github.com/pubgo/g/xcmds"
 	"github.com/pubgo/gitsync/version"
 	"github.com/spf13/cobra"
+	"runtime"
 )
 
 func init() {
@@ -13,9 +14,12 @@ func init() {
 		Aliases: []string{"v"},
 		Short:   "version info",
 		Run: func(cmd *cobra.Command, args []string) {
-			fmt.Println("version", version.Version)
-			fmt.Println("commitV", version.CommitV)
-			fmt.Println("buildV", version.BuildV)
+			fmt.Println("Version:", version.Version)
+			fmt.Println("GitHash:", version.CommitV)
+			fmt.Println("BuildDate:", version.BuildV)
+			fmt.Println("GoVersion:", runtime.Version())
+			fmt.Println("GOOS:", runtime.GOOS)
+			fmt.Println("GOARCH:", runtime.GOARCH)
 		},
 	})
 }
--- version/version.go
+++ version/version.go
@@ -2,4 +2,3 @@ package version
 const Version = "v0.1.6"
 const BuildV = "2019-11-19 15:12:02"
 const CommitV = "f88bd5d6"
-var GoVersion=""
